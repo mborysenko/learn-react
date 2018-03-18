066d70bdadd52eba76e611e8fd5648d920b5116a
Maksym Borysenko <mborysenko@sdl.com>
2018-03-18 19:12:52 +0200

    Improved default exporting

--- src/pages/AboutPage/AboutPage.js
+++ src/pages/AboutPage/AboutPage.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export class AboutPage extends React.Component {
+export default class AboutPage extends React.Component {
     render() {
         return (
             <div className={"jumbotron"}>
@@ -10,5 +10,3 @@ export class AboutPage extends React.Component {
         );
     }
 }
-
-export default AboutPage;
