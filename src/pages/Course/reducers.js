67b3d29a5c079a47a2b0e5a4e2102747fdc537cd
Maksym Borysenko <mborysenko@sdl.com>
2018-03-18 19:16:15 +0200

    Reducers are now using action types from constants

--- src/pages/Course/reducers.js
+++ src/pages/Course/reducers.js
@@ -1,8 +1,12 @@
-import { CREATE_COURSE_ACTION } from "./actions";
+import { Actions } from "./constants";
 
 export default function courseReducer(state = [], action) {
+    if(action.type === Actions.COURSES_LOAD_SUCCESS_ACTION) {
+        return action.courses;
+    }
+
     let newState = state;
-    if(action.type === CREATE_COURSE_ACTION) {
+    if(action.type === Actions.CREATE_COURSE_ACTION) {
         newState = [
             ...state,
             Object.assign({}, action.course)
